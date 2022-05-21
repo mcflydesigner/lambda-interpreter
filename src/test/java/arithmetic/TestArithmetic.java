54444a84c831235359b0a657808c636cbc2eb382
Vladislav.Lamzenkov <v.lamzenkov@planeta.ru>
2022-05-22 00:28:59 +0300

    Added support for syntax auto-tests. Added errorStream support

--- src/test/java/arithmetic/TestArithmetic.java
+++ src/test/java/arithmetic/TestArithmetic.java
@@ -11,28 +11,43 @@ import static utils.TestUtils.getInputStream;
 public class TestArithmetic {
 
     @Test
-    public void testSimpleSummation() throws IOException {
-        OutputStream outputStream = new java.io.ByteArrayOutputStream();
+    public void testSimpleSummation() throws Exception {
         InputStream inputStream = getInputStream("tests/arithmetic/test_simple_summation.nk");
+        OutputStream errorStream = new ByteArrayOutputStream();
+        OutputStream outputStream = new ByteArrayOutputStream();
 
-        MainTest.test(inputStream, outputStream);
+        MainTest.test(inputStream, errorStream, outputStream);
 
         String expected = "11".trim();
         String output = outputStream.toString().trim().replaceAll("\r\n", "\n");
 
         Assert.assertEquals(expected, output);
+        Assert.assertTrue(errorStream.toString().isEmpty());
     }
 
     @Test
-    public void testSimpleSubtraction() throws IOException {
-        OutputStream outputStream = new java.io.ByteArrayOutputStream();
+    public void testSimpleSubtraction() throws Exception {
         InputStream inputStream = getInputStream("tests/arithmetic/test_simple_subtraction.nk");
+        OutputStream errorStream = new ByteArrayOutputStream();
+        OutputStream outputStream = new ByteArrayOutputStream();
 
-        MainTest.test(inputStream, outputStream);
+        MainTest.test(inputStream, errorStream, outputStream);
 
         String expected = "-1".trim();
         String output = outputStream.toString().trim().replaceAll("\r\n", "\n");
 
         Assert.assertEquals(expected, output);
+        Assert.assertTrue(errorStream.toString().isEmpty());
+    }
+
+    @Test
+    public void testSimpleSummationWrongTypes() throws Exception {
+        InputStream inputStream = getInputStream("tests/arithmetic/test_simple_summation_wrong_types.nk");
+        OutputStream errorStream = new ByteArrayOutputStream();
+        OutputStream outputStream = new ByteArrayOutputStream();
+
+        MainTest.test(inputStream, errorStream, outputStream);
+
+        Assert.assertFalse(errorStream.toString().isEmpty());
     }
 }
