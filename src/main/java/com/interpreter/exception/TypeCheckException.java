e2e44bc8e0735b08f4c44459a4b8b490751e8eb1
e2xen <max.ks1230@gmail.com>
2022-05-26 07:04:09 +0300

    basic type-checking, imports to do

--- src/main/java/com/interpreter/exception/TypeCheckException.java
+++ src/main/java/com/interpreter/exception/TypeCheckException.java
@@ -1,6 +1,6 @@
 package com.interpreter.exception;
 
-public abstract class TypeCheckException extends RuntimeException {
+public class TypeCheckException extends RuntimeException {
 
     public TypeCheckException(String message) {
         super(message);
