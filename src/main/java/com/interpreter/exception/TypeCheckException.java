4d83a9a2ea0db1f6675e0809e18ba7c941345258
e2xen <max.ks1230@gmail.com>
2022-05-26 09:48:24 +0300

    add imports

--- src/main/java/com/interpreter/exception/TypeCheckException.java
+++
@@ -1,9 +0,0 @@
-package com.interpreter.exception;
-
-public class TypeCheckException extends RuntimeException {
-
-    public TypeCheckException(String message) {
-        super(message);
-    }
-
-}
